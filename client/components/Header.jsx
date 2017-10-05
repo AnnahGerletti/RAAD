867564844b53215b6cfd23309ae2f660ee74a5c4
Your Name <you@example.com>
2017-10-06 08:25:52 +1300

    Fixed up classname - className

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <section classname="section">
-      <div classname="container">
-        <h1 classname="title">Bulma title class</h1>
-        <h1 classname="title">Bulmas css not working in jsx</h1>
+    <section className="section">
+      <div className="container">
+        <h1 className="title">Bulma Title</h1>
       </div>
     </section>
   )
